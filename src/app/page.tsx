d8a8d3c7ba15117840af884911bf2e28fec67aa8
Ankush <obviouslyankush@gmail.com>
2024-08-16 01:28:54 +0530

    fixed : a bug in dropdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
 'use client';
-import { useEffect } from 'react';
+import { createContext, useContext } from 'react';
 import { useAtom } from 'jotai';
 import { atomDarkMode } from './_state/atom';
 import Navbar from './_components/navbar';
 
 export default function Home() {
   const [darkMode,_] = useAtom(atomDarkMode);
-
+  
   
   return (
+    
     <main className={darkMode==true?"dark":""}>
       <Navbar/>
       
       <div className=''></div>
     </main>
+    
   );
 }
